2fd61168c67127e289b5393d409a1f4e309974a4
Artem Ustimov <art@ustimov.org>
2017-08-11 13:00:22 +0300

    External API calls via requester

--- acs/repository/reviewer.py
+++ acs/repository/reviewer.py
@@ -74,15 +74,13 @@ class Requester:
 
     @retry(stop=stop_after_attempt(SETTINGS['attempt']),
            wait=wait_exponential(multiplier=SETTINGS['multiplier'], max=SETTINGS['max']))
-    def create_comment(self, pull_request, commit, text, file, line):
+    def create_comment(self, pull_request, text, commit, file, line):
         pull_request.create_comment(text, commit, file, line)
 
 
-# TODO: Retrying for all external API requests
 class Reviewer:
-    def __init__(self, access_token, logger):
-        self.access_token = access_token
-        self.github = Github(self.access_token)
+    def __init__(self, requester, logger):
+        self.requester = requester
         self.logger = logger
 
     # def run_command(command):
@@ -94,25 +92,20 @@ class Reviewer:
 
     def get_repositories(self, username):
         self.logger.info("Obtaining of {0}'s repositories was started".format(username))
-        repositories = self.github.get_user(username).get_repos()
+        repositories = self.requester.get_repositories(username)
         self.logger.info('Repositories was obtained')
         return repositories
 
     def create_pull_request_hook(self, username, repository_name, callback_url):
         self.logger.info('Setting of pull request web hook was started')
-        github_repository = self.github.get_user(username).get_repo(repository_name)
-        github_repository.create_hook('web', {'url': callback_url, 'content_type': 'json'}, ['pull_request'], True)
+        self.requester.create_pull_request_hook(username, repository_name, callback_url)
         self.logger.info('Pull request web hook was successfully set')
 
-    #
-    #
     # def is_need_to_handle_hook(body):
     #     # We need to check repo only if pull request is newly created or received a new commit.
     #     if ACTION in body and (body[ACTION] == OPENED or body[ACTION] == SYNCHRONIZE):
     #         return True
     #     return False
-    #
-    #
 
     def clone_repository(self, clone_url, save_path):
         self.logger.info('Repository cloning from {0} to {1} was started'.format(clone_url, save_path))
@@ -120,13 +113,13 @@ class Reviewer:
             self.logger.info('Directory {0} already exists. It will be deleted'.format(save_path))
             shutil.rmtree(save_path)
         bytes_io = io.BytesIO()
-        porcelain.clone(clone_url, save_path, errstream=bytes_io)
+        self.requester.clone_repository(clone_url, save_path, bytes_io)
         self.logger.info(bytes_io.getvalue().decode())
         self.logger.info('Repository was cloned successfully')
 
     def get_file(self, file_url, save_path):
         self.logger.info('File downloading from {0} to {1} was started'.format(file_url, save_path))
-        file = requests.get(file_url)
+        file = self.requester.get_file(file_url)
         self.logger.info('File {0} was downloaded successfully'.format(file_url))
         with open(save_path, 'w') as f:
             f.write(file.content.decode())
@@ -160,15 +153,16 @@ class Reviewer:
     def get_pull_request_and_latest_commit(self, username, repository_name, pull_request_number):
         self.logger.info('Obtaining of pull request with number {0} from repository {1}/{2} was started'.format(
             pull_request_number, username, repository_name))
-        pull_request = self.github.get_user(username).get_repo(repository_name).get_pull(pull_request_number)
-        self.logger.info('Pull request with name {0} was obtained\nObtaining of latest commit was started'.format(pull_request.title))
-        commit = pull_request.get_commits().reversed[0]
+        pull_request = self.requester.get_pull_request(username, repository_name, pull_request_number)
+        self.logger.info('Pull request with name {0} was obtained\nObtaining of latest commit was started'
+                         .format(pull_request.title))
+        commit = self.requester.get_latest_commit_from_pull_request(pull_request)
         self.logger.info('Commit with sha {0} was obtained'.format(commit.sha))
         return pull_request, commit
 
     def review_pull_request(self, metrics, repository_path, diff_path, pull_request, commit):
         self.logger.info('Review was started')
-        commit.create_status('pending', SETTINGS['url'], 'Review was started', SETTINGS['name'])
+        self.requester.create_status(commit, 'pending', SETTINGS['url'], 'Review was started', SETTINGS['name'])
         analyzer = Analyzer(metrics)
         counter = Counter()
         with open(os.path.join(repository_path, diff_path), 'r') as f:
@@ -190,7 +184,8 @@ class Reviewer:
                         elif 'lines' not in value:
                             sent_inspections[inspection] = True
                             self.logger.info('Issuing comment {0} for file {1}'.format(value['message'], patch.path))
-                            pull_request.create_issue_comment('{0}:\n{1}'.format(patch.path, value['message']))
+                            self.requester.create_issue_comment(pull_request, '{0}:\n{1}'.format(patch.path,
+                                                                                                 value['message']))
                             sent_inspection_count += 1
                         else:
                             for line in value['lines']:
@@ -204,12 +199,15 @@ class Reviewer:
                                                      .format(line_object.diff_line_no, line_object.value, patch.path,
                                                              line, value['message'], hunk.target_start,
                                                              hunk.target_start + hunk.target_length))
-                                    pull_request.create_comment(value['message'], commit, patch.path, target_line)
+                                    self.requester.create_comment(pull_request, value['message'], commit, patch.path,
+                                                                  target_line)
                                     sent_inspection_count += 1
         if sent_inspection_count == 0:
-            commit.create_status('success', SETTINGS['url'], 'Review was completed. No issues found', SETTINGS['name'])
+            self.requester.create_status(commit, 'success', SETTINGS['url'], 'Review was completed. No issues found',
+                                         SETTINGS['name'])
         else:
-            commit.create_status('error', SETTINGS['url'], 'Review was completed. Found some issues', SETTINGS['name'])
+            self.requester.create_status(commit, 'error', SETTINGS['url'], 'Review was completed. Found some issues',
+                                         SETTINGS['name'])
         self.logger.info('Review was completed. {0} issues found'.format(sent_inspection_count))
 
     #
